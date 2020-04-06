2e7adc0fa268463e39e05ac7623bf60235bb0d3b
ShrikantVerma <43176895+ShrikantVerma@users.noreply.github.com>
2020-04-06 15:11:43 +0530

    Update ping.sh

--- ping.sh
+++ ping.sh
@@ -10,3 +10,35 @@ do
     echo "node $output is Unreachable" >> ping_fail.txt
     fi
 done
+
+#################################################
+
+#!/bin/bash
+
+for i in $( cat $HOME/iplist.txt )
+do
+ping -q -c2 $i > /dev/null
+if [ $? -eq 0 ]
+then
+echo $i "Pingable"
+else
+echo $i "Not Pingable"
+fi
+done
+
+@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@##############
+
+ #!/bin/bash
+#
+#
+while read line
+do
+        /usr/sbin/ping $line > /dev/null
+        if [ $? == 0 ];
+        then
+        echo "$line is pinging"
+        else
+        echo "$line is not pinging"
+        fi
+done < ipreport.txt
+
